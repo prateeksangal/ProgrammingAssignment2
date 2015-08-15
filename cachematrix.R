c6045a162e3fdf7b98db4d14de3a9f224b0bead8
psangal <prateek.sangal@live.in>
2015-08-14 21:01:30 -0500

    Repo updated with cachematrix to include functions for inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions to make use of caching in R as a result of 
+## scoping rules in R. It makes use of <<- operator to change variable in parent
+## environment
+## This file is set up for the Programming ASsignment for Week 2 of R Programming
+## class in Coursera
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as input and returns a list of functions 
+## to set,get, inverse the matrix entered as an arguement
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL;
+  set<-function(y){
+    x<<-y;
+    m<<-NULL
+  }
+  
+  get<-function() x
+  setmatrix<-function(solve) m<<-solve
+  getmatrix<-function() m
+  
+  list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes a list of functions as input. It further returns
+## the inverse of the matrix stored as cache if the list of functions
+## contains a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m<-x$getmatrix()
+  if(! is.null(m)){
+    message("Getting Cached Data....")
+    return(m)
+  }
+  
+  matrix<-x$get()
+  m<-solve(matrix,...)
+  x$setmatrix(m)
+  m
+  
+  }
